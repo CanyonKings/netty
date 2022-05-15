3abf01e0e7301d069c7065ab3bb3b4a32a03ec5e
canyonking <13424267521@163.com>
2022-05-15 15:40:57 +0800

    Add chinese comments
    Motivation:
    
    Add chinese comments
    
    Modifications:
    
    Add chinese comments
    
    Result:
    
    Add chinese comments

--- common/src/main/java/io/netty/util/concurrent/Future.java
+++ common/src/main/java/io/netty/util/concurrent/Future.java
@@ -21,141 +21,147 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * The result of an asynchronous operation.
+ * 异步操作的结果。
  */
 @SuppressWarnings("ClassNameSameAsAncestorName")
 public interface Future<V> extends java.util.concurrent.Future<V> {
 
     /**
-     * Returns {@code true} if and only if the I/O operation was completed
-     * successfully.
+     * Returns {@code true} if and only if the I/O operation was completed successfully.
+     * 当且仅当 i/o 操作成功完成时，返回true
      */
     boolean isSuccess();
 
     /**
      * returns {@code true} if and only if the operation can be cancelled via {@link #cancel(boolean)}.
+     * 当且仅当可以通过cancel撤销时，返回true
      */
     boolean isCancellable();
 
     /**
-     * Returns the cause of the failed I/O operation if the I/O operation has
-     * failed.
+     * Returns the cause of the failed I/O operation if the I/O operation has failed.
+     * 当 i/o 操作失败，则返回 i/o 操作失败的原因。
      *
      * @return the cause of the failure.
-     *         {@code null} if succeeded or this future is not
-     *         completed yet.
+     * 返回失败的原因
+     * {@code null} if succeeded or this future is not completed yet.
+     * 如果成功，或者这个未来尚未完成返回null。
      */
     Throwable cause();
 
     /**
-     * Adds the specified listener to this future.  The
-     * specified listener is notified when this future is
-     * {@linkplain #isDone() done}.  If this future is already
-     * completed, the specified listener is notified immediately.
+     * 单个监听器
+     * Adds the specified listener to this future.
+     * 将指定的监听器添加到此future。
+     * The specified listener is notified when this future is {@linkplain #isDone() done}.
+     * 当这个future是完成时，会立即通知指定的侦听器。
+     * If this future is already completed, the specified listener is notified immediately.
+     * 如果这个future已经完成，则立即通知指定的侦听器。
      */
     Future<V> addListener(GenericFutureListener<? extends Future<? super V>> listener);
 
     /**
-     * Adds the specified listeners to this future.  The
-     * specified listeners are notified when this future is
-     * {@linkplain #isDone() done}.  If this future is already
-     * completed, the specified listeners are notified immediately.
+     * 多个监听器
+     * Adds the specified listeners to this future.
+     * The specified listeners are notified when this future is {@linkplain #isDone() done}.
+     * If this future is already completed, the specified listeners are notified immediately.
      */
     Future<V> addListeners(GenericFutureListener<? extends Future<? super V>>... listeners);
 
     /**
+     * 单个监听器
      * Removes the first occurrence of the specified listener from this future.
-     * The specified listener is no longer notified when this
-     * future is {@linkplain #isDone() done}.  If the specified
-     * listener is not associated with this future, this method
-     * does nothing and returns silently.
+     * 从此future移除指定侦听器的第一个匹配项。
+     * The specified listener is no longer notified when this future is {@linkplain #isDone() done}.
+     * 当这个future是完成时，不再通知指定的侦听器。
+     * If the specified listener is not associated with this future, this method does nothing and returns silently.
+     * 如果指定的侦听器与此future没有关联，则此方法不执行任何操作，并静默返回。
      */
     Future<V> removeListener(GenericFutureListener<? extends Future<? super V>> listener);
 
     /**
+     * 多个监听器
      * Removes the first occurrence for each of the listeners from this future.
-     * The specified listeners are no longer notified when this
-     * future is {@linkplain #isDone() done}.  If the specified
-     * listeners are not associated with this future, this method
-     * does nothing and returns silently.
+     * The specified listeners are no longer notified when this future is {@linkplain #isDone() done}.
+     * If the specified listeners are not associated with this future, this method does nothing and returns silently.
      */
     Future<V> removeListeners(GenericFutureListener<? extends Future<? super V>>... listeners);
 
     /**
-     * Waits for this future until it is done, and rethrows the cause of the failure if this future
-     * failed.
+     * Waits for this future until it is done, and rethrows the cause of the failure if this future failed.
+     * 同步等待这个future，直到它完成，并在这个future失败时抛出失败的原因。
      */
     Future<V> sync() throws InterruptedException;
 
     /**
-     * Waits for this future until it is done, and rethrows the cause of the failure if this future
-     * failed.
+     * Waits for this future until it is done, and rethrows the cause of the failure if this future failed.
      */
     Future<V> syncUninterruptibly();
 
     /**
      * Waits for this future to be completed.
-     *
-     * @throws InterruptedException
-     *         if the current thread was interrupted
+     * 等待这个future的完成。
+     * @throws InterruptedException if the current thread was interrupted
+     * 如果当前线程被中断就抛出中断异常
      */
     Future<V> await() throws InterruptedException;
 
     /**
-     * Waits for this future to be completed without
-     * interruption.  This method catches an {@link InterruptedException} and
-     * discards it silently.
+     * Waits for this future to be completed without interruption.
+     * 等待这个future不受干扰地完成。
+     * This method catches an {@link InterruptedException} and discards it silently.
+     * 这个方法捕获一个中断异常并默默地丢弃它。
      */
     Future<V> awaitUninterruptibly();
 
     /**
-     * Waits for this future to be completed within the
-     * specified time limit.
-     *
-     * @return {@code true} if and only if the future was completed within
-     *         the specified time limit
-     *
-     * @throws InterruptedException
-     *         if the current thread was interrupted
+     * Waits for this future to be completed within the specified time limit.
+     * 等待这个future在指定的时间限制内完成。
+     * @return {@code true} if and only if the future was completed within the specified time limit
+     * 当且仅当未来在指定的时限内完成返回ture
+     * @throws InterruptedException if the current thread was interrupted
+     * 如果当前线程被中断就抛出中断异常
      */
     boolean await(long timeout, TimeUnit unit) throws InterruptedException;
 
     /**
-     * Waits for this future to be completed within the
-     * specified time limit.
+     * Waits for this future to be completed within the specified time limit.
      *
-     * @return {@code true} if and only if the future was completed within
-     *         the specified time limit
+     * @return {@code true} if and only if the future was completed within the specified time limit
      *
-     * @throws InterruptedException
-     *         if the current thread was interrupted
+     * @throws InterruptedException if the current thread was interrupted
+     * 如果当前线程被中断就抛出中断异常
      */
     boolean await(long timeoutMillis) throws InterruptedException;
 
     /**
-     * Waits for this future to be completed within the
-     * specified time limit without interruption.  This method catches an
-     * {@link InterruptedException} and discards it silently.
-     *
-     * @return {@code true} if and only if the future was completed within
-     *         the specified time limit
+     * Waits for this future to be completed within the specified time limit without interruption.
+     * 等待此future在指定的时间限制内完成，不中断。
+     * This method catches an {@link InterruptedException} and discards it silently.
+     * 他的方法捕获一个中断异常 ，然后默默地丢弃它。
+     * @return {@code true} if and only if the future was completed within the specified time limit
+     * 当且仅当future在指定的时限内完成返回 true
      */
     boolean awaitUninterruptibly(long timeout, TimeUnit unit);
 
     /**
-     * Waits for this future to be completed within the
-     * specified time limit without interruption.  This method catches an
-     * {@link InterruptedException} and discards it silently.
+     * Waits for this future to be completed within the specified time limit without interruption.
+     * This method catches an {@link InterruptedException} and discards it silently.
      *
-     * @return {@code true} if and only if the future was completed within
-     *         the specified time limit
+     * @return {@code true} if and only if the future was completed within the specified time limit
      */
     boolean awaitUninterruptibly(long timeoutMillis);
 
     /**
-     * Return the result without blocking. If the future is not done yet this will return {@code null}.
+     * Return the result without blocking.
+     * 不阻塞地返回结果。
+     *
+     * If the future is not done yet this will return {@code null}.
+     * 如果未完成未来操作，将返回null。
      *
      * As it is possible that a {@code null} value is used to mark the future as successful you also need to check
      * if the future is really done with {@link #isDone()} and not rely on the returned {@code null} value.
+     * 因为有可能使用null值来标记future是成功的，所以您还需要检查future是否真的用isdone完成，而不是依赖返回的null值。
      */
     V getNow();
 
@@ -163,6 +169,7 @@ public interface Future<V> extends java.util.concurrent.Future<V> {
      * {@inheritDoc}
      *
      * If the cancellation was successful it will fail the future with a {@link CancellationException}.
+     * 如果取消是成功的，它将失败的future与{@link CancellationException }。
      */
     @Override
     boolean cancel(boolean mayInterruptIfRunning);
--- common/src/main/java/io/netty/util/concurrent/Promise.java
+++ common/src/main/java/io/netty/util/concurrent/Promise.java
@@ -17,50 +17,58 @@ package io.netty.util.concurrent;
 
 /**
  * Special {@link Future} which is writable.
+ * 可写的特殊Future
  */
 public interface Promise<V> extends Future<V> {
 
     /**
-     * Marks this future as a success and notifies all
-     * listeners.
+     * Marks this future as a success and notifies all listeners.
+     * 将这个future标记为一个成功，并通知所有的监听器
      *
      * If it is success or failed already it will throw an {@link IllegalStateException}.
+     * 如果它已经成功或失败了，它将抛出一个违法状态异常
      */
     Promise<V> setSuccess(V result);
 
     /**
-     * Marks this future as a success and notifies all
-     * listeners.
-     *
-     * @return {@code true} if and only if successfully marked this future as
-     *         a success. Otherwise {@code false} because this future is
-     *         already marked as either a success or a failure.
+     * Marks this future as a success and notifies all listeners.
+     * 将这个future标记为一个成功，并通知所有的监听器
+     * @return {@code true} if and only if successfully marked this future as a success.
+     *         Otherwise {@code false} because this future is already marked as either a success or a failure.
+     * 返回ture 当且仅当成功地标志着这个future是成功的
+     * 否则返回false，因为这个未来已经被标记为成功或失败
      */
     boolean trySuccess(V result);
 
     /**
-     * Marks this future as a failure and notifies all
-     * listeners.
+     * Marks this future as a failure and notifies all listeners.
+     * 将这个future标记为一个失败，并通知所有的监听器
      *
      * If it is success or failed already it will throw an {@link IllegalStateException}.
+     * 如果它已经成功或失败了，它将抛出一个违法状态异常
      */
     Promise<V> setFailure(Throwable cause);
 
     /**
-     * Marks this future as a failure and notifies all
-     * listeners.
+     * Marks this future as a failure and notifies all listeners.
+     * 将这个future标记为一个失败，并通知所有的监听器
      *
-     * @return {@code true} if and only if successfully marked this future as
-     *         a failure. Otherwise {@code false} because this future is
-     *         already marked as either a success or a failure.
+     * @return {@code true} if and only if successfully marked this future as a failure.
+     * Otherwise {@code false} because this future is already marked as either a success or a failure.
+     * 返回ture 当且仅当成功地标志着这个future是失败的
+     * 否则返回false，因为这个未来已经被标记为成功或失败
      */
     boolean tryFailure(Throwable cause);
 
     /**
      * Make this future impossible to cancel.
+     * 让这个future无法取消
      *
-     * @return {@code true} if and only if successfully marked this future as uncancellable or it is already done
-     *         without being cancelled.  {@code false} if this future has been cancelled already.
+     * @return {@code true} if and only if successfully marked this future as uncancellable or it is already done without being cancelled.
+     *         {@code false} if this future has been cancelled already.
+     * 当且仅当成功地标记这个future 是不可撤销的 或者已经完成而从未撤销返回ture
+     * 如果这个 future 已经被撤销返回false
+     *
      */
     boolean setUncancellable();
 
